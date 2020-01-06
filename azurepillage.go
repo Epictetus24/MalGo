c9c3851ed42053916412baf6e59fe311f18c35df
Epictetus <36168644+Epictetus24@users.noreply.github.com>
2020-01-06 17:28:24 +0000

    Update azurepillage.go

--- azurepillage.go
+++ azurepillage.go
@@ -35,11 +35,44 @@ func walkFn(path string, f os.FileInfo, err error) error {
         return nil
 }
 
-//To add - func for reading file in path and searching for the "azurestrings" regex.
+func stringHunt(path2 string, f2 os.FileInfo, err error) error {
+        if f2.IsDir() {
+                return nil
+        }
+
+
+        fn, err := os.Open(path2)
+        if err != nil {
+                log.Fatalln(err)
+        }
+        defer fn.Close()
+
+        scanner := bufio.NewScanner(fn)
+        azurestrings := []string{"TokenCache", "Tenant", "PublishSettings", "FileURL", "ManagementPortalURL"}
+        azurecnt := len(azurestrings)
+
+        for scanner.Scan() {
+                file := scanner.Text()
+                for x := 0; x < azurecnt; x++ {
+                        str := azurestrings[x]
+                        if strings.Contains(file, str) {
+                                fmt.Printf("[+] Possible JSON profile %s : %s\n", str,path2)
+                        }
+                }
+        }
+        if err := scanner.Err(); err != nil {
+                log.Fatalln(err)
+        }
+        return nil
+}
+
 
 func main() {
         root := os.Args[1]
         if err := filepath.Walk(root, walkFn); err != nil {
+                log.Panicln(err)
+        }
+        if err := filepath.Walk(root, stringHunt); err != nil {
                 log.Panicln(err)
         }
 }
